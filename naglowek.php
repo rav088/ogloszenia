29e4c7a4e66a10b48740cb6188c8f86ab58f1a96
Rafal <rafcyk@op.pl>
2016-05-10 23:47:53 +0200

    Inicjalizacja

---
+++ naglowek.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <title>Sprawdź pesel</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+  <style>
+  .container{
+	width: 90%;
+	margin: 0 auto;
+	margin-top: 10px;
+  }
+  </style>
+</head>
+<body>
